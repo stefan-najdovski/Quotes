92faa863a011d4f5eeaefc69707f3b9a80bde390
Стефан Најдовски <83938539+stefan-najdovski@users.noreply.github.com>
2022-10-11 15:01:42 +0200

    rename Activity to smth appropriate

--- android-sauce-code/app/src/main/java/me/snajdovski/quotes/QuotesActivity.kt
+++ android-sauce-code/app/src/main/java/me/snajdovski/quotes/QuotesActivity.kt
@@ -3,9 +3,9 @@ package me.snajdovski.quotes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class QuotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_quote)
     }
 }
