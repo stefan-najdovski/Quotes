348d382e3527aa9c8f270252ce82c95a1c390e80
Стефан Најдовски <83938539+stefan-najdovski@users.noreply.github.com>
2022-10-11 15:08:42 +0200

    Create Quote.kt

---
+++ android-sauce-code/app/src/main/java/me/snajdovski/quotes/data/Quote.kt
@@ -0,0 +1,7 @@
+package me.snajdovski.quotes.data
+
+data class Quote(val quoteText: String, val author: String) {
+    override fun toString():String{
+        return "$quoteText - $author"
+    }
+}   
