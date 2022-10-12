4e2d084174e4361a60cca7f556bcc17804214d0c
Стефан Најдовски <83938539+stefan-najdovski@users.noreply.github.com>
2022-10-12 18:05:55 +0200

    Refactor QuotesActivity

--- android-sauce-code/app/src/main/java/me/snajdovski/quotes/ui/quotes/QuotesActivity.kt
+++ android-sauce-code/app/src/main/java/me/snajdovski/quotes/ui/quotes/QuotesActivity.kt
@@ -1,14 +1,11 @@
 package me.snajdovski.quotes.ui.quotes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import me.snajdovski.quotes.R
 import me.snajdovski.quotes.data.Quote
 import me.snajdovski.quotes.util.InjectorUtils
@@ -30,15 +27,14 @@ class QuotesActivity : AppCompatActivity() {
 
 
         val factory = InjectorUtils.provideQuotesViewModelDirectory()
-        val viewModel = ViewModelProviders.of(this,factory).get(QuotesViewModel::class.java)
-
-        viewModel.getQuotes().observe(this, Observer { quotes ->
+        val viewModel = ViewModelProvider(this, factory)[QuotesViewModel::class.java]
+        viewModel.getQuotes().observe(this) { quotes ->
             val stringBuilder = StringBuilder()
-            quotes.forEach {quote ->
-            stringBuilder.append("$quote\n\n")
+            quotes.forEach { quote ->
+                stringBuilder.append("$quote\n\n")
             }
             textViewQuotes.text = stringBuilder.toString()
-        })
+        }
 
         buttonAddQuote.setOnClickListener{
             val quote = Quote(editTextQuote.text.toString(),editTextAuthor.text.toString())
